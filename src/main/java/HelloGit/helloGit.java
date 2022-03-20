639c685fe9c0e99ce6047a477900ce2f7d39debd
xiaolai <3134156@qq.com>
2022-03-20 16:48:31 +0800

    创建的分支

--- src/main/java/HelloGit/helloGit.java
+++ src/main/java/HelloGit/helloGit.java
@@ -6,5 +6,6 @@ public class helloGit {
     public static void main(String[] args) {
         System.out.println("更新1");
         System.out.println("更新2");
+        System.out.println("看看分支");
     }
 }
